c98ca537a9cc740cc932492ac4a582c4a31fe971
AmritpalS13 <37382398+AmritpalS13@users.noreply.github.com>
2019-05-23 09:51:40 -0400

    Started Project in react

---
+++ VeloKeeper/App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
+
+//Importing my Components below,
+import HeaderPage from './Components/HeaderPage.js'//THe header page.
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+class ScoreCards extends Component {
+  render() {
+    return(
+
+    );
+  }
+}
+
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={{flex:1, backgroundColor:'#737373'}}>
+        {/*So the ImageBackground, is going to be one that exist for the 
+        entire app, so we must place every Component class between that tag.*/}
+        <ImageBackground source={require('./Assets/cf.png')} style={{flex:1}}>
+          {/*Below is going to be the header for the app.*/}
+          <HeaderPage />        
+
+
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+
+//This style sheet came with first build.
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ VeloKeeper/Components/HeaderPage.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+
+
+/*The following is the header page, it's going to contain no inputs
+    for the user. */
+
+export default class HeaderPage extends Component {
+    render() {
+        return (
+          <View style={headerStyle.container}>
+            <Image source={require('./logo.png')} style={{width:300, height:300}} />
+          </View>
+        );
+    }
+}
+
+const headerStyle = StyleSheet.create({
+    container: {
+        padding:10,
+        alignItems:'center',
+        backgroundColor:'#161514',
+        borderBottomColor:'#fdb722',
+        borderBottomWidth:2,
+    },
+})
