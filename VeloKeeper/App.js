d8f06cea033614b1c68457a152eb408575db5312
AmritpalS13 <37382398+AmritpalS13@users.noreply.github.com>
2019-05-23 16:31:24 -0400

    Added input area
    Did some testing for the inputs that the user can enter, I created the main UI for it.

--- VeloKeeper/App.js
+++ VeloKeeper/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, ImageBackground, Image, Button, ScrollView, StatusBar} from 'react-native';
+import {Platform, StyleSheet, Text, View, ImageBackground, Image, Button, ScrollView, StatusBar, TextInput} from 'react-native';
 
 //Importing my Components below,
 import HeaderPage from './Components/HeaderPage.js'//THe header page.
@@ -21,6 +21,16 @@ const instructions = Platform.select({
 });
 
 
+class CardHandler extends Component {
+  render() {
+    return (
+      <View>
+        <PlayerCards />
+      </View>
+    );
+  }
+}
+
 export default class App extends Component{
   render() {
     return (
@@ -31,16 +41,25 @@ export default class App extends Component{
         <ImageBackground source={require('./Assets/cf.png')} style={{flex:1}}>
           {/*Below is going to be the header for the app.*/}
           <HeaderPage />
-          <Text style={{color:'white', textAlign:'center'}}>Input Area (Not done yet)</Text>        
-          
+            {/*This will be a testing of the input section*/}
+            <View style={inputAreaStyle.container}>
+              <View style={inputAreaStyle.inputs}>
+                <TextInput placeholderTextColor='white' style={{paddingLeft:15}}placeholder="Enter Name" />
+                <TextInput placeholderTextColor='white' style={{paddingRight:1}}placeholder="Enter Bike #" />
+                <TextInput placeholderTextColor='white' style={{paddingRight:15}}placeholder="Enter Time" />
+              </View>
+            <Button color='firebrick' title="Submit" /> 
+            </View>
+            {/* <Button title="Submit" />         */}
           <ScrollView>
-            <PlayerCards />
-            <PlayerCards />
-            <PlayerCards />
-            <PlayerCards />
-            <PlayerCards />
-            <PlayerCards />
-            <PlayerCards />
+            <CardHandler />
+            <CardHandler />
+            <CardHandler />
+            <CardHandler />
+            <CardHandler />
+            <CardHandler />
+            <CardHandler />
+            <CardHandler />
           </ScrollView>
 
         </ImageBackground>
@@ -50,7 +69,21 @@ export default class App extends Component{
 }
 
 
-
+const inputAreaStyle = StyleSheet.create({
+  container: {
+    paddingBottom:8,
+    // flexDirection:'row',
+    backgroundColor:'rgba(52,52,52,0.8)',
+    // alignItems:'center',
+    // justifyContent:'space-between'
+  },
+  inputs: {
+    color:'white',
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between'
+  } 
+})
 //This style sheet came with first build.
 const styles = StyleSheet.create({
   container: {
